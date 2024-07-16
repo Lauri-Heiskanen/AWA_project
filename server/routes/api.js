433a0654de16d40ef9fe6521b30859f23979c3b9
LatexGlovesLmao <106094206+Lauri-Heiskanen@users.noreply.github.com>
2024-07-16 17:33:46 +0300

    stuff

--- server/routes/api.js
+++ server/routes/api.js
@@ -7,11 +7,11 @@ const validateToken = require("../auth/validateToken");
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+  res.send("api is up and running");
 });
 
 router.post(
-  "/api/user/register",
+  "/register",
   body("name").trim().isLength({ min: 1, max: 256 }).isAlpha().withMessage("invalid name"),
   body("email").trim().isEmail().withMessage("invalid email"),
   body("password").isStrongPassword({ minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1 }),
@@ -43,14 +43,14 @@ router.post(
   }
 );
 
-router.post("/api/user/login", (req, res) => {
+router.post("/login", (req, res) => {
   try {
-    User.findOne({ email: req.body.email }).then((newUser) => {
-      if (bcrypt.compareSync(req.body.password, newUser.password) && req.body.password) {
+    User.findOne({ email: req.body.email }).then((foundUser) => {
+      if (bcrypt.compareSync(req.body.password, foundUser.password) && req.body.password) {
         jwt.sign(
           {
-            id: newUser._id,
-            email: newUser.email,
+            id: foundUser._id,
+            email: foundUser.email,
           },
           process.env.SECRET,
           { expiresIn: 120 },
@@ -66,5 +66,68 @@ router.post("/api/user/login", (req, res) => {
     console.log(err);
   }
 });
+
+router.get("/newUser", validateToken, (req, res) => {
+  try {
+    User.findOne({ _id: req.user.id }).then((foundUser) => {
+      User.find({}, (users) => {
+        if (foundUser.likedUsers.length + foundUser.dislikedUsers.length < users.length) {
+          let newUser = users[randomNumber(0, users.length)];
+          while (foundUser.dislikedUsers.include(newUser._id) || foundUser.likedUsers.include(newUser._id)) {
+            newUser = users[randomNumber(0, users.length)];
+          }
+          res.status(200).json({ name: newUser.name, description: newUser.description });
+        } else if (foundUser.likedUsers.length < users.length) {
+          let newUser = users[randomNumber(0, users.length)];
+          while (foundUser.dislikedUsers.include(newUser)) {
+            newUser = users[randomNumber(0, users.length)];
+          }
+          res.status(200).json({ name: newUser.name, description: newUser.description });
+        } else {
+          res.status(404).send("no viable users found");
+        }
+      });
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.post("/like", validateToken, (req, res) => {
+  try {
+    User.findOne({ _id: req.user.id }).then((foundUser) => {
+      const index = foundUser.dislikedUsers.indexOf(req.body.targetId);
+      if (index >= 0) {
+        foundUser.dislikedUsers.splice(index, 1);
+      }
+      foundUser.likedUsers.push(req.body.targetId);
+      foundUser.save();
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.post("/dislike", validateToken, (req, res) => {
+  try {
+    User.findOne({ _id: req.user.id }).then((foundUser) => {
+      const index = foundUser.likedUsers.indexOf(req.body.targetId);
+      if (index >= 0) {
+        foundUser.likedUsers.splice(index, 1);
+      }
+      foundUser.dislikedUsers.push(req.body.targetId);
+      foundUser.save();
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+function randomNumber(min, max) {
+  if (max > min) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+  return null;
+}
 
 module.exports = router;
